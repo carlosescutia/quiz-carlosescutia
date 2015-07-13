36dc515ef3e02df1ed11569181a23dfccd4311e9
Carlos Escutia <carlosescutia@gmail.com>
2015-07-13 11:42:34 -0500

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,4 @@
 var models = require('../models/models.js');
-var QS = require('querystring');
 
 // Autoload - factoriza el codigo si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
@@ -19,7 +18,7 @@ exports.index = function(req, res) {
   if (req.query.search) {
     search = ('%' + req.query.search + '%').replace(' ', '%');
     console.log(search);
-    models.Quiz.findAll({where: ["pregunta like ?", search], order: ["pregunta"]}).then(
+    models.Quiz.findAll({where: ["lower(pregunta) like lower(?)", search], order: ["pregunta"]}).then(
       function(quizes) {
         res.render('quizes/index.ejs', {quizes: quizes});
       }
@@ -41,7 +40,7 @@ exports.show = function(req, res) {
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
   models.Quiz.find(req.params.quizId).then(function(quiz) {
-    if (req.query.respuesta === req.quiz.respuesta) {
+    if (req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase()) {
       res.render('quizes/answer', 
           { quiz: quiz, respuesta: 'Correcto'});
     } else {
@@ -51,3 +50,22 @@ exports.answer = function(req, res) {
   })
 };
 
+
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build( // crea objeto quiz
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+
+  res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+  var quiz = models.Quiz.build( req.body.quiz );
+
+  // guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');
+  })  // Redireccion HTTP (URL relativo) lista de preguntas
+};
