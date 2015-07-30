b26bd2c9978acf60cd61eedf7094d6545e02fc43
Carlos Escutia <carlosescutia@gmail.com>
2015-07-30 13:56:48 -0500

    Validacion de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -7,9 +7,9 @@ exports.load = function(req, res, next, quizId) {
       if (quiz) {
         req.quiz = quiz;
         next();
-      } else { next(new Error('No existe quizId=' + quizId)); }
+      } else { next(new Error('No existe quizId=' + quizId)) }
     }
-  ).catch(function(error) { next(error);});
+  ).catch(function(error) { next(error)});
 };
 
 // GET /quizes
@@ -20,13 +20,13 @@ exports.index = function(req, res) {
     console.log(search);
     models.Quiz.findAll({where: ["lower(pregunta) like lower(?)", search], order: ["pregunta"]}).then(
       function(quizes) {
-        res.render('quizes/index.ejs', {quizes: quizes});
+        res.render('quizes/index.ejs', {quizes: quizes, errors: []});
       }
     ).catch(function(error) {next(error); });
   } else {
     models.Quiz.findAll().then(
       function(quizes) {
-        res.render('quizes/index.ejs', { quizes: quizes});
+        res.render('quizes/index.ejs', { quizes: quizes, errors: []});
       }
     ).catch(function(error) { next(error); })
   }
@@ -34,7 +34,7 @@ exports.index = function(req, res) {
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz});
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -42,10 +42,10 @@ exports.answer = function(req, res) {
   models.Quiz.find(req.params.quizId).then(function(quiz) {
     if (req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase()) {
       res.render('quizes/answer', 
-          { quiz: quiz, respuesta: 'Correcto'});
+          { quiz: req.quiz, respuesta: 'Correcto', errors: []});
     } else {
       res.render('quizes/answer', 
-          { quiz: quiz, respuesta: 'Incorrecto'});
+          { quiz: req.quiz, respuesta: 'Incorrecto', errors: []});
     }
   })
 };
@@ -57,15 +57,24 @@ exports.new = function(req, res) {
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
 
-  // guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  })  // Redireccion HTTP (URL relativo) lista de preguntas
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')})
+      } // res.redirect: Redireccion HTTP (URL relativo) lista de preguntas
+    }
+  );
 };
